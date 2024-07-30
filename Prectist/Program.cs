046a46b29c0f17525f322220656a172b3dd18f6d
Vishal Prajapati <vishalp951295@gmail.com>
2024-07-30 09:46:22 +0530

    Add project files.

---
+++ Prectist/Program.cs
@@ -0,0 +1,86 @@
+﻿
+//Finding palindrome name 
+
+//Console.WriteLine("enter name");
+//string name = Console.ReadLine();
+//string name2 = "";
+
+//for(int i=name.Length-1; i>=0; i--)
+//{
+//    name2 = name2 + name[i];
+//}
+//Console.WriteLine(name2);
+//if (name2 == name)
+//{
+//    Console.WriteLine(name + " is palindrome name");
+//}
+
+
+//finding digit and alfabate
+
+//char a = Convert.ToChar(Console.ReadLine());
+//if (char.IsDigit(a))
+//{
+//    Console.WriteLine(a+" is an digit");
+//}
+//else if (char.IsLetter(a))
+//{
+//    Console.WriteLine(a + " is an alfabate");
+//}
+
+
+//string a = "12542724524514";
+//char b =Convert.ToChar(Console.ReadLine());
+//int count = 0;
+//foreach (char c in a)
+//{
+//    if(c == b)
+//    {
+//    count++; 
+//    }
+//}
+//Console.WriteLine(b+" is come in a "+count + " time");
+
+//int[] a = [25, 26, 24, 84, 62, 42];
+//Console.WriteLine(a.Length);
+
+
+//printing revearce array 
+
+//int[] a =new int[10];
+
+//int[] b = new int[10];
+
+//for(int i = 0; i<10; i++)
+//{
+//    a[i] = Convert.ToInt32(Console.ReadLine());
+//}
+//int c = 0;
+//for(int j = a.Length - 1; j >= 0; j--)
+//{
+//    b[c] = a[j];
+//    c++;
+//}
+
+//Console.WriteLine("revears array is ");
+//for(int i = 0; i < b.Length; i++)
+//{
+//    Console.WriteLine(b[i]);
+//}
+
+
+//finding sum of an array number
+
+//int[] num = new int[10];
+//int sum = 0;
+//for (int i = 0; i < num.Length; i++)
+//{
+//    num[i] = Convert.ToInt32(Console.ReadLine());
+//}
+//for (int i = 0;i < num.Length; i++)
+//{
+//    sum += num[i];
+//}
+//Console.WriteLine("total numbers sum is "+sum);
+
+
